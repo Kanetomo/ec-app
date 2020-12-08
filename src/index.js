8edc180625ed626f30707595d50c55eb09aed728
mac <mac@macnoMacBook-ea.local>
2020-12-09 08:01:20 +0900

    re-ducks thunk

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
+import {ConnectedRouter} from 'connected-react-router';
+import * as History from "history";
 import createStore from'./reducks/store/store';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-export const store = createStore();
+const history = History.createBrowserHistory();
+export const store = createStore(history);
 
 ReactDOM.render(
   <Provider store={store}>
+    <ConnectedRouter history={history}>
     <App />
+    </ConnectedRouter>
   </Provider>,
   document.getElementById('root')
 );
